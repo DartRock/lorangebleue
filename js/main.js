0dcf11ffc3bfd8635a284b718a834bcf47d9c36c
DartRock <dartrockofficial@gmail.com>
2023-01-08 17:33:28 +0100

    test

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// console.log('zalupa');
+
+
+// const user = {
+//     firstName: "name",
+//     lastName: "lastname",
+//     phone: "+380995456745",
+//     email: "email@email.com",
+//   };
+  
+//   const options = {
+//     method: "POST",
+//     body: JSON.stringify(user),
+//     headers: {
+//       "Content-Type": "application/json; charset=UTF-8",
+//     },
+//   };
+  
+//   fetch("https://63badc0932d17a50907f94d7.mockapi.io/users", options)
+//     .then(response => response.json())
+//     .then(post => console.log(post))
+//     .catch(error => console.log(error));
